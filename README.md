340f29cb0a3872eafc1e65f153cb1396533fca04
anais-labit <anais.labit@gmail.com>
2023-10-19 17:01:09 +0200

    responsive

--- README.md
+++ README.md
@@ -1,9 +1,6 @@
 # React + Vite
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
-
-Currently, two official plugins are available:
-
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
 # calculator9000
+
+job01, job02, job03 :
+Variations autour de https://github.com/madzadev/calculator/blob/master/README.md
--- src/components/jsx/Button.jsx
+++ src/components/jsx/Button.jsx
@@ -1,6 +1,26 @@
+import React from "react";
 import "../css/Button.css";
 
-const Button = ({ className, value, onClick }) => {
+const Button = ({ value, onClick }) => {
+  let className = "";
+
+  if (value === "=") {
+    className = "equals";
+  } else if (
+    value === "+-" ||
+    value === "%" ||
+    value === "X" ||
+    value === "-" ||
+    value === "+" ||
+    value === "/"
+  ) {
+    className = "operator";
+  } else if (value === "C") {
+    className = "reset";
+  } else {
+    className = "";
+  }
+
   return (
     <button className={className} onClick={onClick}>
       {value}
